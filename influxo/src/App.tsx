61197b60e32f74046ff06f3577393e5e84e1453a
Florian Berisha <florian.fani@hotmail.com>
2024-09-15 00:47:57 +0200

    second commit, fixed tailwind and transitioned to typescript

---
+++ influxo/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-700 font-bold text-5xl">
+        This is where it all begins
+      </h1>
+    </div>
+  );
+}
+
+export default App;
