e866cdcc6d8f36356bb046017c1e78a398f82ec5
Florian Berisha <florian.fani@hotmail.com>
2024-09-23 01:05:45 +0200

    some details

--- influxo/src/components/Footer.tsx
+++ influxo/src/components/Footer.tsx
@@ -124,37 +124,43 @@ export const Footer: React.FC = () => {
           </div>
 
           <div className="flex flex-col justify-between gap-y-2">
-            <a
-              href="#"
-              className="text-sm font-medium bg-[#1DA1F214] rounded-md text-[#061C3D] px-3 py-2 hover:opacity-90 transform hover:scale-105 duration-300"
-            >
-              Facebook
-            </a>
-            <a
-              href="#"
-              target="_blank"
-              className="text-sm font-medium bg-[#0E76A8] rounded-md text-[#FFFFFF] px-3 py-2 hover:opacity-90 transform hover:scale-105 duration-300"
-            >
-              Linkedin
-            </a>
-            <a
-              href="#"
-              target="_blank"
-              className="text-sm font-medium bg-[#1DA1F214] rounded-md text-[#061C3D] px-3 py-2 hover:opacity-90 transform hover:scale-105 duration-300"
-            >
-              X
-            </a>
-            <a
-              href="https://www.instagram.com/influxoks/"
-              target="_blank"
-              className="text-sm font-medium bg-[#1DA1F214] rounded-md text-[#061C3D] px-3 py-2 hover:opacity-90 transform hover:scale-105 duration-300"
+            <div className="text-sm font-medium max-w-[80%] lg:max-w-full flex gap-x-2 items-center bg-[#1DA1F214] rounded-md text-[#061C3D] px-3 py-2 cursor-pointer hover:opacity-90 transform hover:scale-105 duration-300">
+              <i className="fab fa-facebook text-[#1877F2]"></i>
+              <a href="#">Facebook</a>
+            </div>
+            <div className="text-sm font-medium max-w-[80%] lg:max-w-full flex gap-x-2 items-center bg-[#0E76A8] rounded-md text-[#FFFFFF] px-3 py-2 cursor-pointer hover:opacity-90 transform hover:scale-105 duration-300">
+              <i className="fab fa-linkedin"></i>
+              <a href="#" target="_blank">
+                Linkedin
+              </a>
+            </div>
+            <div className="text-sm font-medium max-w-[80%] lg:max-w-full flex gap-x-2 items-center bg-gray-950 rounded-md px-3 py-2 cursor-pointer hover:opacity-90 transform hover:scale-105 duration-300">
+              <a className="mx-auto" href="#" target="_blank">
+                <i className="fab fa-x-twitter text-white"></i>
+              </a>
+            </div>
+            <div
+              className="text-sm font-medium max-w-[80%] lg:max-w-full flex gap-x-2 items-center bg-[#1DA1F214] rounded-md text-[#061C3D] px-3 py-2 cursor-pointer hover:opacity-90 transform hover:scale-105 duration-300"
               style={{
                 background:
                   "linear-gradient(45deg, rgba(255, 214, 0, 0.08) 8.39%, rgba(255, 1, 0, 0.08) 50%, rgba(216, 0, 185, 0.08) 91.61%)",
               }}
             >
-              Instagram
-            </a>
+              <i
+                className="fab fa-instagram text-lg"
+                style={{
+                  background:
+                    "linear-gradient(45deg, #FFDC80, #F56040, #C13584, #833AB4)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                  backgroundClip: "text",
+                  color: "transparent",
+                }}
+              ></i>
+              <a href="https://www.instagram.com/influxoks/" target="_blank">
+                Instagram
+              </a>
+            </div>
           </div>
         </div>
       </div>
