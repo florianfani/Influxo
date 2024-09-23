d2e10464599d980306a980c0f99dc49294c8d2ac
Florian Berisha <florian.fani@hotmail.com>
2024-09-23 16:08:33 +0200

    HomePage Animation done, small fixups

--- influxo/src/components/AboutUs.tsx
+++ influxo/src/components/AboutUs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import man2 from "../assets/decorations/portrait-smiling-2.svg";
+import AboutImage from "../assets/decorations/SVG/chatbot1.svg";
 import cpu from "../assets/decorations/Cpu.svg";
 import stack from "../assets/decorations/Stack.svg";
 
@@ -8,53 +8,33 @@ const AboutUs: React.FC = () => {
     <div className="px-[8%] lg:px-[16%] flex flex-col lg:flex-row justify-between w-full mb-[124px]">
       <div className="left-side lg:w-[50%] lg:pr-[10%]">
         <div>
-          <h2 className="text-[#061C3D] font-bold text-4xl lg:text-[56px]">
-            About Us
-          </h2>
-          <p className="text-[#42526B] mt-4">
-            At Influxo, we specialize in transforming ideas into exceptional web
-            experiences. Our team of experts combines advanced development
-            techniques with creative solutions to deliver websites that not only
-            look great but also perform flawlessly. We’re passionate about
-            crafting digital experiences that elevate your brand and drive
-            success.
-          </p>
+          <h2 className="text-[#061C3D] font-bold text-4xl lg:text-[56px]">About Us</h2>
+          <p className="text-[#42526B] mt-4">At Influxo, we specialize in transforming ideas into exceptional web experiences. Our team of experts combines advanced development techniques with creative solutions to deliver websites that not only look great but also perform flawlessly. We’re passionate about crafting digital experiences that elevate your brand and drive success.</p>
         </div>
         <div className="p-6 bg-[#FFF9EE] mt-8 rounded-2xl">
           <div className="flex items-start mb-4 border-b border-[#9BA4B1]">
             <div className="text-[#E53E3E] text-3xl mr-5">
-              <img src={stack} alt="" className="w-[100px]" />
+              <img src={stack} alt="" className="w-[150px] lg:w-[100px]" />
             </div>
             <div>
-              <h3 className="text-[#061C3D] font-semibold text-lg">
-                Why Influxo Leads the Way in Web Development Excellence
-              </h3>
-              <p className="text-[#42526B] mt-2 mb-5">
-                Our team creates custom solutions that merge advanced technology
-                with creative design, ensuring your website performs seamlessly
-                and captivates your audience.
-              </p>
+              <h3 className="text-[#061C3D] font-semibold text-lg">Why Influxo Leads the Way in Web Development Excellence</h3>
+              <p className="text-[#42526B] mt-2 mb-5">Our team creates custom solutions that merge advanced technology with creative design, ensuring your website performs seamlessly and captivates your audience.</p>
             </div>
           </div>
 
           <div className="flex items-start">
             <div className="text-[#0B63E5] text-2xl mr-4">
-              <img src={cpu} alt="" className="w-[70px]" />
+              <img src={cpu} alt="" className="w-[100px] lg:w-[70px]" />
             </div>
             <div>
-              <h3 className="text-[#061C3D] font-semibold text-lg">
-                Influxo's Edge: How We Deliver Superior Web Development Results
-              </h3>
-              <p className="text-[#42526B] mt-2">
-                Influxo delivers superior web development by combining
-                cutting-edge technology with creative design.
-              </p>
+              <h3 className="text-[#061C3D] font-semibold text-lg">Influxo's Edge: How We Deliver Superior Web Development Results</h3>
+              <p className="text-[#42526B] mt-2">Influxo delivers superior web development by combining cutting-edge technology with creative design.</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="right-side w-[50%] hidden lg:flex items-center justify-center">
-        <img src={man2} alt="Smiling man" className="rounded-lg" />
+      <div className="right-side  lg:flex items-center justify-end">
+        <img src={AboutImage} alt="About Us" className="lg:w-[600px]" />
       </div>
     </div>
   );
--- influxo/src/pages/Home.tsx
+++ influxo/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectsSection from "../components/ProjectsSection";
 import AboutUs from "../components/AboutUs";
 import ServicesHome from "../components/ServicesHome";
@@ -8,66 +8,62 @@ import animationData from "../lotties/animation.json";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
+  const [playAnimation, setPlayAnimation] = useState(true);
 
   const redirect = (path: string) => {
     navigate(path);
   };
 
   const defaultOptions = {
-    loop: true,
-    autoplay: true,
+    loop: true, // Keep looping
+    autoplay: true, // Start playing automatically
     animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPlayAnimation((prevPlayAnimation) => !prevPlayAnimation); // Toggle play and pause every 5 seconds
+    }, 8000);
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, []);
+
   return (
     <div className="">
       <div className="min-h-screen bg-white flex flex-col items-center overflow-hidden">
         <div className="z-10 w-full mx-auto px-[8%] lg:px-[16%] py-12 flex flex-col lg:flex-row items-center justify-between">
           {/* Left Section */}
           <div className="lg:w-1/2">
-            <span className="bg-[#FFE9BD] text-[#061C3D] py-2 px-5 rounded-full text-sm font-semibold tracking-widest">
-              WELCOME TO INFLUXO
-            </span>
+            <span className="bg-[#FFE9BD] text-[#061C3D] py-2 px-5 rounded-full text-sm font-semibold tracking-widest">WELCOME TO INFLUXO</span>
             <h1 className="text-4xl lg:text-5xl font-bold text-[#061C3D] mt-4 leading-tight">
               Web Development &<br />
               Innovative Solutions
             </h1>
-            <p className="text-lg text-gray-600 mt-4">
-              Transforming ideas into exceptional web experiences with
-              cutting-edge development and creative solutions.
-            </p>
-            <button
-              className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300"
-              onClick={() => redirect("/contact")}
-            >
+            <p className="text-lg text-gray-600 mt-4">Transforming ideas into exceptional web experiences with cutting-edge development and creative solutions.</p>
+            <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300" onClick={() => redirect("/contact")}>
               Contact Us
             </button>
           </div>
 
           {/* Right Section - Animation */}
           <div className="lg:w-1/2 md:mt-12 lg:mt-0 md:mr-12">
-            {/* <div className="hidden md:block">
+            <div className="w-[400px] h-[300px] lg:w-[1000px] lg:h-[600px]">
               <Lottie
                 isClickToPauseDisabled={true}
-                options={defaultOptions}
-                height={600}
-                width={1000}
-              />
-            </div> */}
-            <div className=" md:hidden">
-              <Lottie
-                isClickToPauseDisabled={true}
-                options={defaultOptions}
-                height={300}
-                width={400}
+                options={{
+                  ...defaultOptions,
+                  autoplay: playAnimation,
+                }}
+                isPaused={!playAnimation}
               />
             </div>
           </div>
         </div>
       </div>
-      <div className="-mt-56">
+      <div className="-mt-56 lg:-mt-24">
         <AboutUs />
       </div>
       <div>
