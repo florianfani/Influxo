164f1654f808cc3faafdebbe9036c051ea85049c
ArtisAdemi <artis.ademi@gmail.com>
2024-09-26 12:26:25 +0200

    changes

--- influxo/src/main.jsx
+++ influxo/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // Add this line
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
