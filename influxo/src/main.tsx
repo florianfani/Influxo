a3ad5079daeaa3522b4b76707f96bb332adaca62
ArtisAdemi <artis.ademi@gmail.com>
2024-09-26 17:16:29 +0200

    test

--- influxo/src/main.tsx
+++ influxo/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // Add this line
-// test
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
